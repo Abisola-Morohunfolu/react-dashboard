1fa3f48a57864fe5df130993df45c5c80c6eeb60
Abisola-Morohunfolu <amorohunfolu@gmail.com>
2020-02-25 21:52:03 +0100

    added production components

--- src/components/Production/Production.js
+++ src/components/Production/Production.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ProductionTitle from './ProductionTitle';
 import '../../styles/componentsStyles/Production.scss';
+import ProductionSummary from './ProductionSummary';
+import Product from './Product';
 
 const Production = () => {
 	return (
 		<section className="Production">
 			<ProductionTitle />
+			<ProductionSummary />
+			<Product />
 		</section>
 	);
 };
